4571b86f15e0a74bc9022fee8f154f840443861b
Julia Chuprova <nikaniks57@gmail.com>
2023-12-08 14:38:37 +0200

    add register, login and logout

--- picture_palace_hub/settings.py
+++ picture_palace_hub/settings.py
@@ -17,7 +17,6 @@ import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -51,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'users.middlewares.LogoutMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -76,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'picture_palace_hub.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -90,7 +89,6 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -110,23 +108,25 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -137,3 +137,5 @@ AUTH_USER_MODEL = "users.Customer"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+TIME_SINCE_LAST_ACTION = 3 * 60  # seconds
---
+++ users/middlewares.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import logout
+from django.utils import timezone
+from django.utils.deprecation import MiddlewareMixin
+from picture_palace_hub import settings
+import datetime
+
+
+class LogoutMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        if not request.user.is_staff or not request.user.is_superuser:
+            now = timezone.now()
+            last_action_not_found = request.session.get('last_action')
+            if last_action_not_found:
+                last_action = datetime.datetime.strptime(last_action_not_found, "%H-%M-%S %d/%m/%y")
+                if (now - last_action).seconds > settings.TIME_SINCE_LAST_ACTION:
+                    logout(request)
+            request.session['last_action'] = datetime.datetime.now().strftime("%H-%M-%S %d/%m/%y")
