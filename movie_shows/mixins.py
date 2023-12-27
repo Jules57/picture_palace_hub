fe92f246d34d91a72e991e7d0bde125594b43490
Julia Chuprova <nikaniks57@gmail.com>
2023-12-27 20:07:01 +0200

    update SoldTicketMixin without get method

--- movie_shows/mixins.py
+++ movie_shows/mixins.py
@@ -11,19 +11,6 @@ class AdminRequiredMixin(UserPassesTestMixin):
 
 
 class SoldTicketCheckMixin:
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-
-        if isinstance(self.object, MovieShow) and self.object.sold_seats > 0:
-            messages.error(self.request, 'This movie show is already booked.')
-            return HttpResponseRedirect(self.object.get_absolute_url())
-
-        if isinstance(self.object, CinemaHall) and hasattr(self.object, 'shows') and any(
-                show.sold_seats > 0 for show in self.object.shows.all()):
-            messages.error(self.request, 'A movie show in this hall is already booked.')
-            return HttpResponseRedirect(self.object.get_absolute_url())
-
-        return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
